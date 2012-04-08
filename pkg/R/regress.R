baa71668e3d492a1aeb7d6c1d129ce6f583e73f5
yl2 (Yang  Lu) <yang.lu@williams.edu>
2012-04-08 01:27:15 +0000

    reg new
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pa1@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/regress.R
+++ pkg/R/regress.R
@@ -7,7 +7,7 @@ regress <- function(x,
                     ret.var = "return",
                     ## variables on the right hand side of the
                     ## regression formula
-                    reg.var = c("sector", "country", "size", "value", "growth"),
+                    reg.var = c("sector", "value", "growth"),
                     benchmark.weight = "benchmark",
                     portfolio.weight = "portfolio"
                     ){
@@ -108,39 +108,20 @@ regress <- function(x,
     ## the weight.var column must be numeric
     stopifnot(is.numeric(x[[benchmark.weight]]))
     stopifnot(is.numeric(x[[portfolio.weight]]))
-
     
-    ## ## portfolio weight by sector
-    ## weight.port <- tapply(x[[portfolio.weight]], x[[sector.var]], sum)
-    ## ## benchmark weight by sector
-    ## weight.bench <- tapply(x[[bench.weight]], x[[sector.var]], sum)
-    ## reg@weight.port <- weight.port
-    ## reg@weight.bench <- weight.bench
-    
-
     benchmark.ret <- x[[benchmark.weight]] %*% x[[ret.var]]
     portfolio.ret <- x[[portfolio.weight]] %*% x[[ret.var]]
-
     act.ret <- portfolio.ret - benchmark.ret
+
+    var.input <- .formula.make(ret.var, c(reg.var, "- 1"))
     
-    contrib <- NULL
-    reg.coefficients <- list()
-    len <- length(reg.var)
-    for (i in 1:len){
-      ## Create the regression formula. The left hand side is the
-      ## ret.var. The right hand side is the ret.var -- user-defined
-      ## factors
-      f <- .formula.make(ret.var, reg.var[-i])
-      
-      lm.obj <- lm(f, data = x) ## not weighted
-      pred.ret <- x[[portfolio.weight]] %*% predict(lm.obj)
-      
-      ## contribution of a variable
-      contrib[i] <- portfolio.ret - pred.ret
-      reg.coefficients[[i]] <- coefficients(lm.obj)
-    }
-    
-    names(contrib) <- reg.var
+    lm.model <- lm(var.input, data = x)
+    lm.mat <- model.matrix(var.input, data = x)
+    factor.ret <- lm.model$coefficients
+    act.weight <- x[[portfolio.weight]] - x[[benchmark.weight]]
+    act.expo <- apply(act.weight * lm.mat, 2, sum)
+    contrib <- act.expo * factor.ret
+
     
     ## Create a regression object with slots date.var, ret.var,
     ## reg.var, weights, ret.orig, ret.reg, coefficients,
@@ -156,10 +137,11 @@ regress <- function(x,
                universe = x
                )
 
-    reg@coefficients <- reg.coefficients ## list
+    reg@coefficients <- factor.ret
     reg@benchmark.ret <- benchmark.ret
     reg@portfolio.ret <- portfolio.ret
     reg@act.ret <- act.ret
+    reg@act.expo <- act.expo
     reg@contrib <- contrib
 
     return(reg)
