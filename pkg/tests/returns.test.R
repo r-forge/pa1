4adb18d2c33fdc1cca3b168e4f6d9005b405c804
yl2 (Yang  Lu) <yang.lu@williams.edu>
2012-04-17 03:46:03 +0000

    add
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pa1@103 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/returns.test.R
@@ -0,0 +1,43 @@
+## returns.test.R
+## Yang Lu Yang.Lu@williams.edu
+
+library(pa)
+
+## data(jan)
+## b1 <- brinson(x = jan)
+## truth <- returns(b1)
+
+## r1 <- regress(jan)
+## truth.r1 <- returns(r1)
+
+## data(quarter)
+## b2 <- brinson(x = quarter)
+## truth.multi <- returns(b2)
+
+## r2 <- regress(quarter)
+## truth.multi.r2 <- returns(r2)
+
+## save(truth, truth.multi, truth.r1, truth.multi.r2, file = "returns.test.RData")
+
+
+## Single-period
+data(jan)
+b1 <- brinson(x = jan)
+result <- returns(b1)
+stopifnot(all.equal(result, truth))
+
+r1 <- regress(jan)
+result.r1 <- returns(r1)
+stopifnot(all.equal(result.r1, truth.r1))
+
+## Multi-period
+
+data(quarter)
+b2 <- brinson(x = quarter)
+result.multi <- returns(b2)
+stopifnot(all.equal(result.multi, truth.multi))
+
+r2 <- regress(quarter)
+result.multi.r2 <- returns(r2)
+stopifnot(all.equal(result.multi.r2, truth.multi.r2))
+
