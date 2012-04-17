763b61026340874fb2b6e96290e9da16396e3c1b
yl2 (Yang  Lu) <yang.lu@williams.edu>
2012-04-17 03:58:13 +0000

    add
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pa1@106 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/plot.test.R
@@ -0,0 +1,37 @@
+## plot.test.R
+## Yang Lu Yang.Lu@williams.edu
+
+library(pa)
+
+## data(jan)
+## b1 <- brinson(x = jan)
+## truth <- plot(b1)
+
+## data(quarter)
+## b2 <- brinson(x = quarter)
+## truth.multi <- plot(b2)
+
+## r2 <- regress(quarter)
+## truth.multi.r2 <- plot(r2)
+
+## save(truth, truth.multi, truth.multi.r2, file = "plot.test.RData")
+
+
+## Single-period
+data(jan)
+b1 <- brinson(x = jan)
+result <- plot(b1)
+stopifnot(all.equal(result, truth))
+
+
+## Multi-period
+
+data(quarter)
+b2 <- brinson(x = quarter)
+result.multi <- plot(b2)
+stopifnot(all.equal(result.multi, truth.multi))
+
+r2 <- regress(quarter)
+result.multi.r2 <- plot(r2)
+stopifnot(all.equal(result.multi.r2, truth.multi.r2))
+
